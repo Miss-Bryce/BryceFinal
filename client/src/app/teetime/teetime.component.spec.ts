c88f6cee469d5fb4698dea38b94a801588da8e35
J. Bryce <53979775+Miss-Bryce@users.noreply.github.com>
2021-12-15 12:00:50 -0500

    Added Angular

---
+++ client/src/app/teetime/teetime.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TeetimeComponent } from './teetime.component';
+
+describe('TeetimeComponent', () => {
+  let component: TeetimeComponent;
+  let fixture: ComponentFixture<TeetimeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TeetimeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TeetimeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/teetime/teetime.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teetime',
+  templateUrl: './teetime.component.html',
+  styleUrls: ['./teetime.component.css']
+})
+export class TeetimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
